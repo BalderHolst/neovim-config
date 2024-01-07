68dcbbc1aba4dd24befdf827251d6e0e1a52e8b5
BalderHolst <balderwh@gmail.com>
2024-01-07 18:27:11 +0100

    oil!

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,60 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+
+            -- Skip the confirmation popup for simple operations
+            skip_confirm_for_simple_edits = true,
+            -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+            prompt_save_on_select_new_entry = true,
+
+            -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+            -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+            -- Additionally, if it is a string that matches "actions.<name>",
+            -- it will use the mapping at require("oil.actions").<name>
+            -- Set to `false` to remove a keymap
+            -- See :help oil-actions for a list of all available actions
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["<C-v>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+            -- Set to false to disable all of the above keymaps
+            use_default_keymaps = true,
+
+            -- Configuration for the floating progress window
+            progress = {
+                max_width = 0.9,
+                min_width = { 40, 0.4 },
+                width = nil,
+                max_height = { 10, 0.9 },
+                min_height = { 5, 0.1 },
+                height = nil,
+                border = "rounded",
+                minimized_border = "none",
+                win_options = {
+                    winblend = 0,
+                },
+            },
+
+        })
+
+
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
+}
