9bbd0e0e6a93c5a79bed92d25a3bad08fe14e743
BalderHolst <balderwh@gmail.com>
2024-01-15 22:13:18 +0100

    rust

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,13 @@
 return {
     {
+        'saecki/crates.nvim',
+        tag = 'stable',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('crates').setup()
+        end,
+    },
+    {
         'simrat39/rust-tools.nvim',
         config = function()
             local rt = require('rust-tools')
