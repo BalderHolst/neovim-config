9262f41e16c8bff3ac3ade7d2e71fda7cce049c7
BalderHolst <balderwh@gmail.com>
2024-02-12 13:06:05 +0100

    clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,6 +51,8 @@ return {
                 },
             })
 
+            lspconfig.clangd.setup({})
+
             vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
             vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
             vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
